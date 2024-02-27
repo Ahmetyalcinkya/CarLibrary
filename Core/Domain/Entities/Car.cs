332f606c747e70b6ecdb1661e724ecba67f005fd
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-02-27 17:41:08 +0300

    Entities Completed!

---
+++ Core/Domain/Entities/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Car
+    {
+        public int CarID { get; set; }
+        public int BrandID { get; set; }
+        public Brand Brand { get; set; }
+        public string Model { get; set; }
+        public string ListCoverImageUrl { get; set; }
+        public int MileAge { get; set; }
+        public string Transmission {  get; set; }
+        public byte Seat {  get; set; }
+        public byte Luggage { get; set; }
+        public string Fuel { get; set; }
+        public string CarCoverImageUrl { get; set; }
+        public List<CarFeature> CarFeatures { get; set; }
+        public List<CarDescription> CarDescriptions { get; set; }
+        public List<CarPricing> CarPricings { get; set; }
+
+    }
+}
---
+++ Core/Domain/Entities/CarFeature.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class CarFeature
+    {
+        public int CarFeatureID { get; set; }
+        public int CarID { get; set; }
+        public Car Car { get; set; }
+        public int FeatureID { get; set; }
+        public Feature Features { get; set; }
+        public bool isAvailable { get; set; }
+    }
+}
