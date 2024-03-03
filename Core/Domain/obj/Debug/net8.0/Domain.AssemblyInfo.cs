a04e31695fab2202783ee6abc76f30cffd9f7910
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-03-04 00:33:30 +0300

    FrontEnd Created w/MVC

--- Core/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Core/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdd3d578af1074346a24133706f533571808ab33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c07180c3ef22ceafaf4d28dc6845c791d1d4f432")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
