e6f4e6bd501ecc3ea7e9cc0af9c3b10ae8a694a8
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-03-04 01:51:28 +0300

    ViewComponents added!

---
+++ Frontends/WebUI/ViewComponents/UILayoutViewComponents/_ScriptsUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _ScriptsUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
