bc42089fd75d9de7f780f21746d8971c6c214354
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-03-06 16:57:04 +0300

    Service Page updated!

---
+++ Frontends/WebUI/Controllers/ServiceController.cs
@@ -0,0 +1,28 @@
+﻿using Dto.ServiceDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebUI.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ServiceController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7199/api/Services");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
