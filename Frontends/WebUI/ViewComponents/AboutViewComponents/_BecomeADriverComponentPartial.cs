946f4c1271161f7cec63c96cbb5efc747831c6b6
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-03-06 16:37:01 +0300

    Testimonial Section added to about page!

---
+++ Frontends/WebUI/ViewComponents/AboutViewComponents/_BecomeADriverComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.AboutViewComponents
+{
+	public class _BecomeADriverComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
