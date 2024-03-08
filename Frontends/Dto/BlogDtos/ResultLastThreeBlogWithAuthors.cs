0432b104500a617a5c18275eafa609930350d07b
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-03-09 01:38:17 +0300

    Blogs component added! Blog section added to main page!

---
+++ Frontends/Dto/BlogDtos/ResultLastThreeBlogWithAuthors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dto.BlogDtos
+{
+    public class ResultLastThreeBlogWithAuthors
+    {
+        public int BlogID { get; set; }
+        public string Title { get; set; }
+        public int AuthorID { get; set; }
+        public string AuthorName { get; set; }
+        public string CoverImageUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int CategoryID { get; set; }
+    }
+}
---
+++ Frontends/WebUI/ViewComponents/BlogViewComponents/_GetLastThreeBlogsWithAuthorsListComponentPartial.cs
@@ -0,0 +1,27 @@
+﻿using Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebUI.ViewComponents.BlogViewComponents
+{
+    public class _GetLastThreeBlogsWithAuthorsListComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _GetLastThreeBlogsWithAuthorsListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7199/api/Blogs/GetLastThreeBlogsWithAuthorsList");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultLastThreeBlogWithAuthors>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
