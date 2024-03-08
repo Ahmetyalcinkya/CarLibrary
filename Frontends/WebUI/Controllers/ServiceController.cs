ab30b5818744466579769cd58e0543400f499131
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-03-08 15:42:39 +0300

    ServiceController fixed! & Services added to Main page

--- Frontends/WebUI/Controllers/ServiceController.cs
+++ Frontends/WebUI/Controllers/ServiceController.cs
@@ -6,22 +6,9 @@ namespace WebUI.Controllers
 {
     public class ServiceController : Controller
     {
-        private readonly IHttpClientFactory _httpClientFactory;
-        public ServiceController(IHttpClientFactory httpClientFactory)
+        public IActionResult Index()
         {
-            _httpClientFactory = httpClientFactory;
-        }
 
-        public async Task<IActionResult> Index()
-        {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7199/api/Services");
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsonData);
-                return View(values);
-            }
             return View();
         }
     }
