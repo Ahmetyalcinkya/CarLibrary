47328e756a911899e5acd9256dbb0933fc645117
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-03-08 14:01:30 +0300

    Main page Banner updated!

---
+++ Frontends/WebUI/ViewComponents/DefaultViewComponents/_DefaultCoverUILayoutComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using Dto.BannerDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultCoverUILayoutComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DefaultCoverUILayoutComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7199/api/Banners");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBannerDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
