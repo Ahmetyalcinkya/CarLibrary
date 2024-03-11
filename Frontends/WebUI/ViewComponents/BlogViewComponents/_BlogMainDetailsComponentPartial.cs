f49087035cbed9374b8df0ddbbea804f125d1c1b
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-03-12 02:04:06 +0300

    Blog Detail page components completed!

---
+++ Frontends/WebUI/ViewComponents/BlogViewComponents/_BlogMainDetailsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.BlogViewComponents
+{
+    public class _BlogMainDetailsComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        { 
+            return View(); 
+        }
+    }
+}
---
+++ Frontends/WebUI/ViewComponents/CommentViewComponents/_AddCommentComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.CommentViewComponents
+{
+    public class _AddCommentComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
