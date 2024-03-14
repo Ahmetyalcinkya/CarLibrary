3fdb20aca23d39fac12dbbff8b6c5e5180fbdbf3
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-03-14 23:16:06 +0300

    Admin theme added! & Admin Starter Page created!

---
+++ Frontends/WebUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public PartialViewResult AdminHeaderPartial()
+        {
+            return PartialView();
+        }
+    }
+}
