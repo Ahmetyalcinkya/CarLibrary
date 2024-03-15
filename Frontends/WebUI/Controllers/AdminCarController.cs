390f46ee7a29d43c1c9d2ad4268c6ecb042d4938
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-03-15 22:42:36 +0300

    New car form added for saving operation!

--- Frontends/WebUI/Controllers/AdminCarController.cs
+++ Frontends/WebUI/Controllers/AdminCarController.cs
@@ -1,5 +1,7 @@
-﻿using Dto.CarDtos;
+﻿using Dto.BrandDtos;
+using Dto.CarDtos;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 
 namespace WebUI.Controllers
@@ -22,6 +24,22 @@ namespace WebUI.Controllers
                 var values = JsonConvert.DeserializeObject<List<ResultCarWithBrandsDto>>(jsonData);
                 return View(values);
             }
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> CreateCar()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7199/api/Brands");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultBrandDto>>(jsonData);
+            List<SelectListItem> brandValues =  (from value in values
+                                                 select new SelectListItem
+                                                 {
+                                                     Text = value.Name,
+                                                     Value = value.BrandID.ToString()
+                                                 }).ToList();
+            ViewBag.BrandValues = brandValues;
             return View();
         }
     }
