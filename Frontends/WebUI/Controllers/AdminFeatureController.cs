b6cfab1d08a9d24836ee1f49790bda31075678f1
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-03-16 23:53:54 +0300

    Featue page activated!

---
+++ Frontends/WebUI/Controllers/AdminFeatureController.cs
@@ -0,0 +1,27 @@
+﻿using Dto.FeatureDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebUI.Controllers
+{
+    public class AdminFeatureController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AdminFeatureController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7199/api/Features");
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
