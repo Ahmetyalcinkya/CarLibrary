f811569540a7e1782a88dcc31c172d7f79fda19f
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-03-27 23:55:56 +0300

    Admin Comment Controller added! & Comment routing added!

---
+++ Frontends/WebUI/Areas/Admin/Controllers/AdminCommentController.cs
@@ -0,0 +1,32 @@
+﻿using Dto.CommentDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebUI.Areas.Admin.Controllers
+{
+    [Route("Admin/AdminComment")]
+    [Area("Admin")]
+    public class AdminCommentController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AdminCommentController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index/{id}")]
+        public async Task<IActionResult> Index(int id)
+        {
+            ViewBag.v = id;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7199/api/Comments/GetCommentByBlogId?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCommentDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
