9160d1c39955b7e0c52cf7c7ba0db1bd806d24ab
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-03-30 01:06:58 +0300

    Admin SocialMedia page added!

---
+++ Frontends/WebUI/Areas/Admin/Controllers/AdminSocialMediaController.cs
@@ -0,0 +1,93 @@
+﻿using Dto.SocialMediaDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace WebUI.Areas.Admin.Controllers
+{
+    [Route("Admin/AdminSocialMedia")]
+    [Area("Admin")]
+    public class AdminSocialMediaController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AdminSocialMediaController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7199/api/SocialMedias");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSocialMediaDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        [Route("CreateSocialMedia")]
+        public IActionResult CreateSocialMedia()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("CreateSocialMedia")]
+        public async Task<IActionResult> CreateSocialMedia(CreateSocialMediaDto createSocialMedia)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createSocialMedia);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7199/api/SocialMedias", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminSocialMedia", new { area = "Admin" });
+            }
+            return View();
+        }
+        [HttpGet]
+        [Route("UpdateSocialMedia/{id}")]
+        public async Task<IActionResult> UpdateSocialMedia(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7199/api/SocialMedias/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<UpdateSocialMediaDto>(jsonData);
+                return View(value);
+            }
+            return View();
+        }
+        [HttpPost]
+        [Route("UpdateSocialMedia/{id}")]
+        public async Task<IActionResult> UpdateSocialMedia(UpdateSocialMediaDto updateSocialMedia)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateSocialMedia);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7199/api/SocialMedias/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminSocialMedia", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [Route("RemoveSocialMedia/{id}")]
+        public async Task<IActionResult> RemoveSocialMedia(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync("https://localhost:7199/api/SocialMedias?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "AdminSocialMedia", new { area = "Admin" });
+            }
+            return View();
+        }
+    }
+}
