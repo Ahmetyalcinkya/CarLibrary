ee4d2e136d020c63afdb84eb940fe33ff90821cc
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-03-31 02:56:49 +0300

    Statistic Results added!

---
+++ Core/Application/Features/Mediator/Results/StatisticsResults/GetCarsAverageWeeklyPriceQueryResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Mediator.Results.StatisticsResults
+{
+    public class GetCarsAverageWeeklyPriceQueryResult
+    {
+        public decimal AverageWeeklyPrice { get; set; }
+    }
+}
