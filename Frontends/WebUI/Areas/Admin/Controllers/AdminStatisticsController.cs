49f315d1cd6fc1eda3dbbf60e7d65bbcd08f8904
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-03-30 17:48:16 +0300

    Admin statistics page activated!

---
+++ Frontends/WebUI/Areas/Admin/Controllers/AdminStatisticsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Areas.Admin.Controllers
+{
+    [Route("Admin/AdminStatistics")]
+    [Area("Admin")]
+    public class AdminStatisticsController : Controller
+    {
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
