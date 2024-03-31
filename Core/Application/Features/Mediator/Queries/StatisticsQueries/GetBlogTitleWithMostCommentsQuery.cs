192381f8458f53609c4d8e21cbcbbc0d10e3b11c
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-03-31 03:09:16 +0300

    Statistic Queries added!

---
+++ Core/Application/Features/Mediator/Queries/StatisticsQueries/GetBlogTitleWithMostCommentsQuery.cs
@@ -0,0 +1,13 @@
+﻿using Application.Features.Mediator.Results.StatisticsResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Application.Features.Mediator.Queries.StatisticsQueries
+{
+    public class GetBlogTitleWithMostCommentsQuery : IRequest<GetBlogTitleWithMostCommentsQueryResult>
+    {
+    }
+}
