63467ee07beec1e0ac1f071fd188c69e11eaaf03
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-04-01 02:16:30 +0300

    Some of the statistics added to UI!

---
+++ Frontends/Dto/StatisticsDtos/ResultStatisticsDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dto.StatisticsDtos
+{
+    public class ResultStatisticsDto
+    {
+        public int CarCount { get; set; }
+        public int LocationCount { get; set; }
+        public int AuthorCount { get; set; }
+        public int BlogCount { get; set; }
+    }
+}
--- Frontends/WebUI/Areas/Admin/Controllers/AdminStatisticsController.cs
+++ Frontends/WebUI/Areas/Admin/Controllers/AdminStatisticsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dto.StatisticsDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace WebUI.Areas.Admin.Controllers
 {
@@ -6,9 +8,66 @@ namespace WebUI.Areas.Admin.Controllers
     [Area("Admin")]
     public class AdminStatisticsController : Controller
     {
-        [Route("Index")]
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AdminStatisticsController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index")]
+        public async Task<IActionResult> Index()
+        {
+            Random random = new Random();
+            var client = _httpClientFactory.CreateClient();
+
+            #region GetCarCount
+            var responseMessage = await client.GetAsync("https://localhost:7199/api/Statistics/GetCarCount");
+            if(responseMessage.IsSuccessStatusCode) 
+            {
+                int randomNum = random.Next(0, 101);
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
+                ViewBag.carCount = values.CarCount;
+                ViewBag.carCountNumber = randomNum;
+            }
+            #endregion
+
+            #region GetLocationCount
+            var responseMessage2 = await client.GetAsync("https://localhost:7199/api/Statistics/GetLocationCount");
+            if (responseMessage2.IsSuccessStatusCode)
+            {
+                int randomNum = random.Next(0, 101);
+                var jsonData = await responseMessage2.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
+                ViewBag.locationCount = values.LocationCount;
+                ViewBag.locationCountNumber = randomNum;
+            }
+            #endregion
+
+            #region GetAuthorCount
+            var responseMessage3 = await client.GetAsync("https://localhost:7199/api/Statistics/GetAuthorCount");
+            if (responseMessage3.IsSuccessStatusCode)
+            {
+                int randomNum = random.Next(0, 101);
+                var jsonData = await responseMessage3.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
+                ViewBag.authorCount = values.AuthorCount;
+                ViewBag.authorCountNumber = randomNum;
+            }
+            #endregion
+
+            #region GetBlogCount
+            var responseMessage4 = await client.GetAsync("https://localhost:7199/api/Statistics/GetBlogCount");
+            if (responseMessage4.IsSuccessStatusCode)
+            {
+                int randomNum = random.Next(0, 101);
+                var jsonData = await responseMessage4.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ResultStatisticsDto>(jsonData);
+                ViewBag.blogCount = values.BlogCount;
+                ViewBag.blogCountNumber = randomNum;
+            }
+            #endregion
+
             return View();
         }
     }
