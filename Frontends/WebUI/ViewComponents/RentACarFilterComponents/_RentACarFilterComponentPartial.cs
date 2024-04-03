4df257f4e1da6fb99dda987857067b67f0b5d79c
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-04-04 00:30:55 +0300

    Rental form added!

---
+++ Frontends/WebUI/ViewComponents/RentACarFilterComponents/_RentACarFilterComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.RentACarFilterComponents
+{
+    public class _RentACarFilterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { 
+            return View(); 
+        }
+    }
+}
