8ecba9303b4d7d86c85a171302ad86f91bb70770
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-04-04 16:34:17 +0300

    Location data retrieved from the database and listed in the UI.

--- Frontends/WebUI/Controllers/DefaultController.cs
+++ Frontends/WebUI/Controllers/DefaultController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dto.LocationDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 namespace WebUI.Controllers
 {
-    public class DefaultController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class DefaultController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+		public DefaultController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7199/api/Locations");
+
+			var jsonData = await responseMessage.Content.ReadAsStringAsync();
+			var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
+			List<SelectListItem> ddvalues = (from x in values
+											 select new SelectListItem
+											 {
+												 Text = x.Name,
+												 Value = x.LocationID.ToString(),
+											 }).ToList();
+			ViewBag.ddvalues = ddvalues;
+			return View();
+		}
+	}
 }
