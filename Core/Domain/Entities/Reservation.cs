07aba8f12bbe37e4caf8d3212fce617d962cec03
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-04-05 23:46:59 +0300

    Reservation Entity added!

---
+++ Core/Domain/Entities/Reservation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Reservation
+    {
+        public int ReservationID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int CarID { get; set; }
+        // public string PickUpLocationID { get; set; }
+        public int Age { get; set; }
+        public int DriverLicenceYear { get; set; }
+        public string? Description { get; set; }
+    }
+}
