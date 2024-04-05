f9733545309d49e9719a34a82f0d4b4e7619964a
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-04-05 23:42:20 +0300

    Reservation form and Reservation controller added!

---
+++ Frontends/WebUI/Controllers/ReservationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class ReservationController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Rent the Car";
+            ViewBag.v2 = "Reservation form for the Car Rental";
+            return View();
+        }
+    }
+}
