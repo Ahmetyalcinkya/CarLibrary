cc7dd98a888dc02f4e0cca71c94bfc7564d7f54c
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-04-06 16:52:00 +0300

    Form fixed & location information added to dropdown!

--- Frontends/WebUI/Controllers/ReservationController.cs
+++ Frontends/WebUI/Controllers/ReservationController.cs
@@ -1,13 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dto.LocationDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json;
 
 namespace WebUI.Controllers
 {
     public class ReservationController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ReservationController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
         {
             ViewBag.v1 = "Rent the Car";
             ViewBag.v2 = "Reservation form for the Car Rental";
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7199/api/Locations");
+
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultLocationDto>>(jsonData);
+            List<SelectListItem> ddvalues = (from x in values
+                                             select new SelectListItem
+                                             {
+                                                 Text = x.Name,
+                                                 Value = x.LocationID.ToString(),
+                                             }).ToList();
+            ViewBag.ddvalues = ddvalues;
             return View();
         }
     }
