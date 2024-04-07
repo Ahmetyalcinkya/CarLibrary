2914fc76431f011f00a997127b70d112ba1ea494
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-04-08 00:00:07 +0300

    Pricing table added to UI!

---
+++ Frontends/Dto/CarPricingDtos/ResultCarPricingListWithModelDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dto.CarPricingDtos
+{
+	public class ResultCarPricingListWithModelDto
+	{
+        public string Model { get; set; }
+        public decimal DailyAmount { get; set; }
+        public decimal WeeklyAmount { get; set; }
+        public decimal MonthlyAmount { get; set; }
+    }
+}
--- Frontends/WebUI/Controllers/CarPricingController.cs
+++ Frontends/WebUI/Controllers/CarPricingController.cs
@@ -1,14 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dto.CarPricingDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace WebUI.Controllers
 {
     public class CarPricingController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+		public CarPricingController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IActionResult> Index()
         {
             ViewBag.v1 = "Pricings";
             ViewBag.v2 = "Choose a Car Pricing";
-            return View();
-        }
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7199/api/CarPricings/GetCarPricingsWithTimePeriods");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCarPricingListWithModelDto>>(jsonData);
+				return View(values);
+			}
+			return View();
+		}
     }
 }
