6debe593a51e64bddae08670af7a5fb250b72ca5
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-04-07 17:47:58 +0300

    Pricing page added to ui!

---
+++ Frontends/WebUI/Controllers/CarPricingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class CarPricingController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.v1 = "Pricings";
+            ViewBag.v2 = "Choose a Car Pricing";
+            return View();
+        }
+    }
+}
