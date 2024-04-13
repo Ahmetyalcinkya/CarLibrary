cbb96809a54c7275f7e046940cf881471e7d6491
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-04-13 21:55:03 +0300

    Blogs added to admin dashboard!

---
+++ Frontends/WebUI/ViewComponents/DashboardViewComponents/_AdminDashboardBlogListComponentPartial.cs
@@ -0,0 +1,27 @@
+﻿using Dto.BlogDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebUI.ViewComponents.DashboardViewComponents
+{
+    public class _AdminDashboardBlogListComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _AdminDashboardBlogListComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7199/api/Blogs/GetAllBlogsWithAuthor");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultAllBlogsWithAuthorDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
