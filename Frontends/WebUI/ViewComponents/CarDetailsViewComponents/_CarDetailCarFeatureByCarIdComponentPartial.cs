83c48a394392843c45651ddae9c59da8c31a1357
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-04-14 22:24:12 +0300

    Car Features listed in UI!

---
+++ Frontends/WebUI/ViewComponents/CarDetailsViewComponents/_CarDetailCarFeatureByCarIdComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using Dto.CarFeatureDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebUI.ViewComponents.CarDetailsViewComponents
+{
+    public class _CarDetailCarFeatureByCarIdComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _CarDetailCarFeatureByCarIdComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [Route("Index/{id}")]
+        [HttpGet]
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            ViewBag.CarId = id;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7199/api/CarFeatures?id=" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultCarFeatureByCarIdDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
---
+++ Frontends/WebUI/ViewComponents/CarDetailsViewComponents/_CarDetailTabPaneComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.CarDetailsViewComponents
+{
+    public class _CarDetailTabPaneComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        { 
+            return View(); 
+        }
+    }
+}
