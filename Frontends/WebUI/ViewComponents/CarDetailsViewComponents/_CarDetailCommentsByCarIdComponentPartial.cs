ac8c2f2e4c637079ab016779d21ba51bb23e4964
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-04-17 14:17:23 +0300

    Reviews added to UI!

--- Frontends/WebUI/ViewComponents/CarDetailsViewComponents/_CarDetailCommentsByCarIdComponentPartial.cs
+++ Frontends/WebUI/ViewComponents/CarDetailsViewComponents/_CarDetailCommentsByCarIdComponentPartial.cs
@@ -1,13 +1,13 @@
-﻿using Dto.CarFeatureDtos;
+﻿using Dto.ReviewDtos;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
 namespace WebUI.ViewComponents.CarDetailsViewComponents
 {
-    public class _CarDetailCarFeatureByCarIdComponentPartial : ViewComponent
+    public class _CarDetailCommentsByCarIdComponentPartial : ViewComponent
     {
         private readonly IHttpClientFactory _httpClientFactory;
-        public _CarDetailCarFeatureByCarIdComponentPartial(IHttpClientFactory httpClientFactory)
+        public _CarDetailCommentsByCarIdComponentPartial(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
@@ -16,11 +16,11 @@ namespace WebUI.ViewComponents.CarDetailsViewComponents
         {
             ViewBag.CarId = id;
             var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:7199/api/CarFeatures?id=" + id);
+            var responseMessage = await client.GetAsync("https://localhost:7199/api/Reviews?id=" + id);
             if (responseMessage.IsSuccessStatusCode)
             {
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultCarFeatureByCarIdDto>>(jsonData);
+                var values = JsonConvert.DeserializeObject<List<ResultReviewByCarIdDto>>(jsonData);
                 return View(values);
             }
             return View();
