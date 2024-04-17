8d48c07f838c9814a30356efe369003a5e9becd8
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-04-17 13:19:38 +0300

    Car description added to UI!

---
+++ Frontends/WebUI/ViewComponents/CarDetailsViewComponents/_CarDetailCarDescriptionByCarIdComponentPartial.cs
@@ -0,0 +1,28 @@
+﻿using Dto.CarDescriptionDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace WebUI.ViewComponents.CarDetailsViewComponents
+{
+    public class _CarDetailCarDescriptionByCarIdComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _CarDetailCarDescriptionByCarIdComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            ViewBag.CarId = id;
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7199/api/CarDescriptions?carID={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<ResultCarDescriptionByCarIdDto>(jsonData);
+                return View(value);
+            }
+            return View();
+        }
+    }
+}
---
+++ Frontends/WebUI/ViewComponents/CarDetailsViewComponents/_CarDetailDescriptionComponentPartial.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.ViewComponents.CarDetailsViewComponents
+{
+    public class _CarDetailDescriptionComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _CarDetailDescriptionComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
