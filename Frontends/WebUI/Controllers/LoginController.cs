ea3f555c148ecdaf1baa005f1b52c088efb8e278
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-04-18 12:55:32 +0300

    Login page added to UI!

---
+++ Frontends/WebUI/Controllers/LoginController.cs
@@ -0,0 +1,30 @@
+﻿using Dto.LoginDtos;
+using Dto.RegisterDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace WebUI.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public LoginController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateLoginDto createLoginDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            return View();
+        }
+    }
+}
